9457f32e9a56e2aa5946f57fc4d30fef29fbc171
Nada39 <Nadamd24@outlook.com>
2023-10-09 17:41:54 +0300

    Upload project

---
+++ src/localtrainticketingsystem/LocalTrainTicketingSystem.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package localtrainticketingsystem;
+
+/**
+ *
+ * @author Nadam_2kg0od8
+ */
+public class LocalTrainTicketingSystem {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        LoginPage lp = new LoginPage();
+        lp.show();
+    }
+    
+}
